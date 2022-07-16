00d6d72a4ad9a9ceef695e805f6a3a2f3d22e750
Jesper Torp Kristensen <jespertk@gmail.com>
2022-07-16 11:27:48 +0200

    A first simple version seems to be working now

--- main.ts
+++ main.ts
@@ -1,36 +1,43 @@
-function init_map () {
-    mapdata = []
-    add_row("11111011111")
-    add_row("10001000001")
-    add_row("10111111101")
-    add_row("10101010101")
-    add_row("11101110101")
-    add_row("10000000101")
-    add_row("10111011101")
-    add_row("10100000001")
-    add_row("10111010111")
-    add_row("10000010001")
-    add_row("11111011111")
-}
+let monster_x = 0
+let pos_x = 0
+let monster_y = 0
+let pos_y = 0
+let dead = false
+let is_playing = false
+let monster_leds: number[] = []
+let time = 0
+let level = 0
+let can_move = false
+let wallcolor = 0
+let outsidecolor = 0
+let unknowncolor = 0
+let mapdata: number[][] = []
+let start_x = 0
+let start_y = 0
+let end_x = 0
+let end_y = 0
+let tmp_array: number[] = []
+let tmp_int = 0
 function tegn_monster () {
-    if (-2 <= monster_x - pos_x && monster_x - pos_x <= 2) {
-        if (-2 <= monster_y - pos_y && monster_y - pos_y <= 2) {
-            led.plotBrightness(monster_x - pos_x + 2, monster_y - pos_y + 2, monster_leds[monster_led_index])
-            monster_led_index = (monster_led_index + 1) % 8
-            music.playSoundEffect(music.createSoundEffect(
-            WaveShape.Noise,
-            5000,
-            100,
-            255,
-            213,
-            100,
-            SoundExpressionEffect.None,
-            InterpolationCurve.Linear
-            ), SoundExpressionPlayMode.InBackground)
+    if (Math.abs(monster_x - pos_x) <= 2 && Math.abs(monster_y - pos_y) <= 2) {
+        if (pos_x == monster_x && pos_y == monster_y) {
+            dead = true
+            is_playing = false
         }
+        led.plotBrightness(monster_x - pos_x + 2, monster_y - pos_y + 2, monster_leds[time % 8])
+        music.playSoundEffect(music.createSoundEffect(
+        WaveShape.Noise,
+        5000,
+        100,
+        255,
+        213,
+        100,
+        SoundExpressionEffect.None,
+        InterpolationCurve.Linear
+        ), SoundExpressionPlayMode.InBackground)
     }
 }
-function completed2 () {
+function failure () {
     for (let xx = 0; xx <= 4; xx++) {
         for (let yy = 0; yy <= 4; yy++) {
             led.plot(xx, yy)
@@ -55,90 +62,38 @@ function completed2 () {
     basic.pause(1000)
 }
 function restart () {
+    level = 1
+    dead = false
+    init_level()
     is_playing = true
-    pos_y = max_y
-    pos_x = row_size
-    monster_x = 1
-    monster_y = 7
-    monster_led_index = 0
     tegn()
 }
+function init_level_2 () {
+    add_row("11111411111")
+    add_row("10001000001")
+    add_row("10111111101")
+    add_row("10101010101")
+    add_row("11101110101")
+    add_row("10000000101")
+    add_row("10111011101")
+    add_row("15100000001")
+    add_row("10111010111")
+    add_row("10000010001")
+    add_row("11111311111")
+}
+function can_move_to (xx: number, yy: number) {
+    return lookup(xx, yy) != 1 && lookup(xx, yy) != 2
+}
 input.onGesture(Gesture.LogoUp, function () {
-    if (is_playing) {
+    if (can_move) {
         go_down()
     }
 })
 input.onGesture(Gesture.TiltLeft, function () {
-    if (is_playing) {
+    if (can_move) {
         go_left()
     }
 })
-function init_map_old () {
-    mapdata_old = []
-    add_row_old([
-    0,
-    1,
-    0,
-    0,
-    -1
-    ])
-    add_row_old([
-    0,
-    1,
-    1,
-    1,
-    0
-    ])
-    add_row_old([
-    1,
-    1,
-    1,
-    1,
-    -1
-    ])
-    add_row_old([
-    1,
-    0,
-    1,
-    0,
-    0
-    ])
-    add_row_old([
-    0,
-    0,
-    0,
-    1,
-    -1
-    ])
-    add_row_old([
-    0,
-    1,
-    0,
-    1,
-    0
-    ])
-    add_row_old([
-    1,
-    0,
-    0,
-    0,
-    -1
-    ])
-    add_row_old([
-    0,
-    1,
-    0,
-    0,
-    1
-    ])
-    add_row_old([
-    0,
-    0,
-    1,
-    0,
-    -1
-    ])
-}
 function draw (screen_x: number, screen_y: number) {
     if (map(screen_x, screen_y) < 2) {
         if (map(screen_x, screen_y) == 0) {
@@ -155,42 +110,15 @@ function draw (screen_x: number, screen_y: number) {
     }
 }
 function go_left () {
-    if (pos_x > 0) {
-        if (map_old(1, 2) == 0) {
-            pos_x = pos_x - 1
+    if (!(finished_level())) {
+        if (can_move_to(pos_x - 1, pos_y)) {
+            pos_x += -1
         }
         tegn()
     }
 }
-function map_old (screen_x: number, screen_y: number) {
-    x = pos_x + screen_x - 2
-    y = pos_y + screen_y - 2
-    if (x == 0 || x == max_x) {
-        return 1
-    }
-    if (y == 0 || y == max_y) {
-        if (x == 5) {
-            return 0
-        } else {
-            return 1
-        }
-    }
-    if (y < 0 || y > max_y) {
-        return 2
-    }
-    if (x % 2 == 0) {
-        if (y % 2 == 0) {
-            return 1
-        } else {
-            return mapdata_old[(y - 1) * row_size + (Math.floor(x / 2) - 1)]
-        }
-    } else {
-        if (y % 2 == 0) {
-            return mapdata_old[(y - 1) * row_size + Math.floor(x / 2)]
-        } else {
-            return 0
-        }
-    }
+function is_not_wall (xx: number, yy: number) {
+    return lookup(xx, yy) != 1
 }
 function tegn () {
     led.plotBrightness(2, 2, 255)
@@ -202,7 +130,7 @@ function tegn () {
     draw(3, 1)
     draw(3, 2)
     draw(3, 3)
-    if (map_old(1, 2) != 1) {
+    if (is_not_wall(1, 2)) {
         draw(0, 1)
         draw(0, 2)
         draw(0, 3)
@@ -211,7 +139,7 @@ function tegn () {
         hide(0, 2)
         hide(0, 3)
     }
-    if (map_old(3, 2) != 1) {
+    if (is_not_wall(3, 2)) {
         draw(4, 1)
         draw(4, 2)
         draw(4, 3)
@@ -220,7 +148,7 @@ function tegn () {
         hide(4, 2)
         hide(4, 3)
     }
-    if (map_old(2, 1) != 1) {
+    if (is_not_wall(2, 1)) {
         draw(1, 0)
         draw(2, 0)
         draw(3, 0)
@@ -229,7 +157,7 @@ function tegn () {
         hide(2, 0)
         hide(3, 0)
     }
-    if (map_old(2, 3) != 1) {
+    if (is_not_wall(2, 3)) {
         draw(1, 4)
         draw(2, 4)
         draw(3, 4)
@@ -238,22 +166,22 @@ function tegn () {
         hide(2, 4)
         hide(3, 4)
     }
-    if (map_old(1, 1) != 1) {
+    if (is_not_wall(1, 1)) {
         draw(0, 0)
     } else {
         hide(0, 0)
     }
-    if (map_old(1, 3) != 1) {
+    if (is_not_wall(1, 3)) {
         draw(0, 4)
     } else {
         hide(0, 4)
     }
-    if (map_old(3, 1) != 1) {
+    if (is_not_wall(3, 1)) {
         draw(4, 0)
     } else {
         hide(4, 0)
     }
-    if (map_old(3, 3) != 1) {
+    if (is_not_wall(3, 3)) {
         draw(4, 4)
     } else {
         hide(4, 4)
@@ -263,7 +191,66 @@ function tegn () {
 function hide (screen_x: number, screen_y: number) {
     led.plotBrightness(screen_x, screen_y, unknowncolor)
 }
-function completed () {
+function init_level () {
+    monster_x = -10
+    monster_y = -10
+    mapdata = []
+    if (level == 1) {
+        init_level_1()
+    }
+    if (level == 2) {
+        init_level_2()
+    }
+    time = 0
+    pos_x = start_x
+    pos_y = start_y
+}
+function map (screen_x: number, screen_y: number) {
+    return lookup(pos_x + screen_x - 2, pos_y + screen_y - 2)
+}
+function go_right () {
+    if (!(finished_level())) {
+        if (can_move_to(pos_x + 1, pos_y)) {
+            pos_x += 1
+        }
+        tegn()
+    }
+}
+function init_level_1 () {
+    add_row("141")
+    add_row("1051")
+    add_row("101")
+    add_row("131")
+}
+function finished_level () {
+    return pos_x == end_x && pos_y == end_y
+}
+function go_up () {
+    if (!(finished_level())) {
+        if (can_move_to(pos_x, pos_y - 1)) {
+            pos_y += -1
+        }
+        tegn()
+    }
+}
+function go_down () {
+    if (!(finished_level())) {
+        if (can_move_to(pos_x, pos_y + 1)) {
+            pos_y += 1
+        }
+        tegn()
+    }
+}
+function lookup (xx: number, yy: number) {
+    if (yy < 0 || yy >= mapdata.length) {
+        return 2
+    }
+    if (xx < 0 || xx >= mapdata[yy].length) {
+        return 2
+    }
+    return mapdata[yy][xx]
+}
+function success () {
     for (let xx = 0; xx <= 4; xx++) {
         for (let yy = 0; yy <= 4; yy++) {
             led.plot(xx, yy)
@@ -287,114 +274,93 @@ function completed () {
     }
     basic.pause(1000)
 }
-function map (screen_x: number, screen_y: number) {
-    y = pos_y + screen_y - 2
-    if (y < 0 || y >= mapdata.length) {
-        return 2
-    }
-    x = pos_x + screen_x - 2
-    if (x < 0 || x >= mapdata[y].length) {
-        return 2
-    }
-    return mapdata[y][x]
-}
-function go_right () {
-    if (pos_x < max_x) {
-        if (map_old(3, 2) == 0) {
-            pos_x = pos_x + 1
-        }
-        tegn()
-    }
-}
-function add_row_old (row: number[]) {
-    for (let value of row) {
-        mapdata_old.push(value)
-    }
-}
-function go_up () {
-    if (pos_y > 0) {
-        if (map_old(2, 1) == 0) {
-            pos_y = pos_y - 1
-        }
-        tegn()
-        if (pos_y == 0) {
-            is_playing = false
-        }
-    }
-}
-function go_down () {
-    if (pos_y < max_y) {
-        if (map_old(2, 3) == 0) {
-            pos_y = pos_y + 1
-        }
-        tegn()
-    }
-}
 input.onGesture(Gesture.TiltRight, function () {
-    if (is_playing) {
+    if (can_move) {
         go_right()
     }
 })
 function add_row (data: string) {
     tmp_array = []
     for (let index = 0; index <= data.length - 1; index++) {
-        tmp_array.push(parseFloat(data.charAt(index)))
+        tmp_int = parseFloat(data.charAt(index))
+        if (tmp_int == 3) {
+            start_x = index
+            start_y = mapdata.length
+            tmp_int = 0
+        }
+        if (tmp_int == 4) {
+            end_x = index
+            end_y = mapdata.length
+            tmp_int = 0
+        }
+        if (tmp_int == 5) {
+            monster_x = index
+            monster_y = mapdata.length
+            tmp_int = 0
+        }
+        tmp_array.push(tmp_int)
     }
     mapdata.push(tmp_array)
 }
 input.onGesture(Gesture.LogoDown, function () {
-    if (is_playing) {
+    if (can_move) {
         go_up()
     }
 })
-let tmp_array: number[] = []
-let y = 0
-let x = 0
-let mapdata_old: number[] = []
-let is_playing = false
-let monster_led_index = 0
-let monster_y = 0
-let pos_x = 0
-let monster_x = 0
-let mapdata: number[][] = []
-let monster_leds: number[] = []
-let outsidecolor = 0
-let unknowncolor = 0
-let wallcolor = 0
-let pos_y = 0
-let max_y = 0
-let max_x = 0
-let row_size = 0
-row_size = 5
-max_x = 2 * row_size
-max_y = 10
-pos_y = max_y
-wallcolor = 150
-unknowncolor = 25
-outsidecolor = 5
-monster_leds = [
-255,
-223,
-127,
-32,
-0,
-32,
-127,
-223
-]
-init_map()
-init_map_old()
-restart()
-basic.forever(function () {
-    while (outsidecolor == 0) {
-        basic.pause(100)
-    }
-    while (true) {
-        while (pos_y != 0) {
-            basic.pause(100)
-            tegn_monster()
+function level_completed () {
+    for (let xx = 0; xx <= 4; xx++) {
+        for (let yy = 0; yy <= 4; yy++) {
+            led.plot(xx, yy)
         }
-        completed()
+    }
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . #
+        . . . # #
+        # . # # .
+        # # # . .
+        . # . . .
+        `)
+    basic.pause(500)
+}
+basic.forever(function () {
+    wallcolor = 150
+    unknowncolor = 25
+    outsidecolor = 5
+    monster_leds = [
+    255,
+    223,
+    127,
+    32,
+    0,
+    32,
+    127,
+    223
+    ]
+    while (true) {
         restart()
+        while (is_playing) {
+            can_move = true
+            while (is_playing && !(finished_level())) {
+                basic.pause(100)
+                tegn_monster()
+                time += 1
+            }
+            can_move = false
+            if (!(dead)) {
+                if (level < 2) {
+                    level_completed()
+                    level += 1
+                    init_level()
+                } else {
+                    is_playing = false
+                }
+            }
+        }
+        if (dead) {
+            failure()
+        } else {
+            success()
+        }
     }
 })
